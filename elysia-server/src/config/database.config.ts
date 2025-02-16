f7328671779c37484f99d4dcb789e61cc613a520
Reverse7K <zakoki711@gmail.com>
2025-02-16 21:16:42 +0700

    update

--- elysia-server/src/config/database.config.ts
+++ elysia-server/src/config/database.config.ts
@@ -10,7 +10,7 @@ export const MongoDB = {
     connect: async function () {
         try {
             await mongoose.connect(uri)
-            console.log('👨‍💻👨‍💻----- MongoDB connected -----👨‍💻👨‍💻')
+            console.log('✅✅✅----- MongoDB connected -----✅✅✅')
 
         } catch (error) {
             console.error('----- MongoDB connection error -----')
