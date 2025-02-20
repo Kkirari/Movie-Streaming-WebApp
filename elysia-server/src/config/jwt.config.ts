60270b300cd1cf68e3614e3d392fa3382c4c48ed
Rawit101 <sorawitardpru@gmail.com>
2025-02-20 15:30:13 +0700

    pagination 99%

--- elysia-server/src/config/jwt.config.ts
+++ elysia-server/src/config/jwt.config.ts
@@ -1,4 +1,4 @@
-import jwt from "@elysiajs/jwt";
+import jwt from "@elysiajs/jwt"
 
 export const jwtConfig = jwt({
     name: 'jwt',
--- elysia-server/src/controllers/account.controller.ts
+++ elysia-server/src/controllers/account.controller.ts
@@ -1,7 +1,7 @@
-import Elysia from "elysia";
-import { jwtConfig } from "../config/jwt.config";
-import { _accountToken, AccountDto } from "../types/account.type";
-import { AccountService } from "../service/account.service";
+import Elysia from "elysia"
+import { jwtConfig } from "../config/jwt.config"
+import { _accountToken, AccountDto } from "../types/account.type"
+import { AccountService } from "../service/account.service"
 
 export const AccountController = new Elysia({
     prefix: "/api/account",
@@ -15,14 +15,14 @@ export const AccountController = new Elysia({
         "/login",
         async ({ body, jwt, set }) => {
             try {
-                const user = await AccountService.login(body);
-                const token = await jwt.sign({ id: user.username });
-                return { user, token };
+                const user = await AccountService.login(body)
+                const token = await jwt.sign({ id: user.username })
+                return { user, token }
             } catch (error) {
-                set.status = "Bad Request";
-                if (error instanceof Error) throw new Error(error.message);
-                set.status = "Internal Server Error";
-                throw new Error("Some Thing went wrong , try again later");
+                set.status = "Bad Request"
+                if (error instanceof Error) throw new Error(error.message)
+                set.status = "Internal Server Error"
+                throw new Error("Some Thing went wrong , try again later")
             }
         },
         {
@@ -36,14 +36,14 @@ export const AccountController = new Elysia({
         "/register",
         async ({ body, jwt, set }) => {
             try {
-                const user = await AccountService.createNewUser(body);
-                const token = await jwt.sign({ id: user.username });
-                return { token, user };
+                const user = await AccountService.createNewUser(body)
+                const token = await jwt.sign({ id: user.username })
+                return { token, user }
             } catch (error) {
-                set.status = "Bad Request";
-                if (error instanceof Error) throw new Error(error.message);
-                set.status = 500;
-                throw new Error("Something wen wrong, try again later");
+                set.status = "Bad Request"
+                if (error instanceof Error) throw new Error(error.message)
+                set.status = 500
+                throw new Error("Something wen wrong, try again later")
             }
         },
         {
@@ -54,12 +54,12 @@ export const AccountController = new Elysia({
             },
 
             beforeHandle: ({ body: { username, password }, set }) => {
-                const usernameRegex = /^[A-Za-z][A-Za-z\d]{3,19}$/;
-                const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$/;
+                const usernameRegex = /^[A-Za-z][A-Za-z\d]{3,19}$/
+                const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,16}$/
                 if (!usernameRegex.test(username) || !passwordRegex.test(password)) {
-                    set.status = "Bad Request";
-                    throw new Error(`Invalid username or password`);
+                    set.status = "Bad Request"
+                    throw new Error(`Invalid username or password`)
                 }
             },
         }
-    );
+    )
